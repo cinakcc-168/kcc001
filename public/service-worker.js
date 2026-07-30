4ca438a26a72ce449fc4921a907a53ba079356b8
kheavchinchhay168-blip <292078204+kheavchinchhay168-blip@users.noreply.github.com>
2026-07-30 16:28:28 +0700

    Add files via upload

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tiny-pos-v35-shell-1";
+const CACHE_VERSION = "tiny-pos-v36-shell-1";
 const SHELL_CACHE = `${CACHE_VERSION}-app`;
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 
