096e6514594fb7d5dff4ceb4deee8684c4cce0ee
kheavchinchhay168-blip <292078204+kheavchinchhay168-blip@users.noreply.github.com>
2026-07-30 16:54:25 +0700

    Add files via upload

--- src/lib/release.js
+++ src/lib/release.js
@@ -1,3 +1,3 @@
-export const APP_RELEASE = "tiny-pos-v1.1.0-beta1-step38";
-export const APP_SCHEMA_STEP = 38;
-export const APP_RELEASE_LABEL = "Tiny POS v1.1 Beta 1";
+export const APP_RELEASE = "tiny-pos-v1.1.0-beta2-step39";
+export const APP_SCHEMA_STEP = 39;
+export const APP_RELEASE_LABEL = "Tiny POS v1.1 Beta 2";
