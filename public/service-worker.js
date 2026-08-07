b6cc11b9f40fa38b42835082d1e3748c229c7aa5
cinakcc-168 <cinakcc@gmail.com>
2026-08-07 18:53:17 +0700

    Add files via upload

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "tiny-pos-step-46-4-25-date-range-presets";
+const CACHE_VERSION = "tiny-pos-step-46-4-26-print-quote-sync";
 const SHELL_CACHE = `${CACHE_VERSION}-app`;
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 
